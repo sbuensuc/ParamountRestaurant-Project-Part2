5f70f96fa40218a280a8f756ae198956f5cb6be0
Sam <Sam@DESKTOP-O0S9A91>
2016-12-09 18:50:21 -0500

    updated added shopping cart functionality

--- ParamountRestaurant-Project-Part2/App_Start/BundleConfig.cs
+++ ParamountRestaurant-Project-Part2/App_Start/BundleConfig.cs
@@ -23,12 +23,14 @@ namespace ParamountRestaurant_Project_Part2
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                   "~/Scripts/custom.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
-                   "~/Scripts/custom.js"));
+            
         }
     }
 }
--- ParamountRestaurant-Project-Part2/Scripts/custom.js
+++ ParamountRestaurant-Project-Part2/Scripts/custom.js
@@ -5,6 +5,33 @@
 
 //Custom JS code
 
+// IIFE - executes immediately
+(function () {
+
+    // Document.ready -> link up remove event handler
+    $(".RemoveLink").click(function () {
+        // Get the id from the link
+        var recordToDelete = $(this).attr("data-id");
+        if (recordToDelete != '') {
+            // Perform the ajax post
+            $.post("/ShoppingCart/RemoveFromCart", { "id": recordToDelete },
+                function (data) {
+                    // Successful requests get here
+                    // Update the page elements
+                    if (data.ItemCount == 0) {
+                        $('#row-' + data.DeleteId).fadeOut('slow');
+                    } else {
+                        $('#item-count-' + data.DeleteId).text(data.ItemCount);
+                    }
+                    $('#cart-total').text(data.CartTotal);
+                    $('#update-message').text(data.Message);
+                    $('#cart-status').text('Cart (' + data.CartCount + ')');
+                });
+        }
+    });
+})();
+
+
 //JS to handle image modal
 $(function () {
     $('.pop').on('click', function () {
@@ -12,3 +39,5 @@ $(function () {
         $('#imagemodal').modal('show');
     });
 });
+
+
